d235c484adb1f738787994edb9d005f7d1f64fbf
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-10-23 23:26:26 -0500

    http GET request json function

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//  GET userlist
+router.get('/userlist', function(req,res){
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.find({},{},function(e, docs){
+		res.json(docs);
+	});
 });
+
+// NOTE: if you do an HTTP GET to /users/userlist, 
+//our server will return JSON that lists all of the users in the database. 
 
 module.exports = router;
