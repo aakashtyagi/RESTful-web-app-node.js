c088cdc8f633acd6746841d93b4d3dca9e69071c
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-10-24 00:32:29 -0500

    adding userinfo

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
 
 	// populate the user table on intial page load
 	populateTable();
+	// Username link click
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 function populateTable(){
@@ -14,6 +16,8 @@ function populateTable(){
 
 	// jQuery AJAX call for JSON
 	$.getJSON('/users/userlist', function(data){
+
+		userListData = data;
 
 		// for each item in JSON, add a new row and cells to the table
 		$.each(data, function(){
@@ -28,3 +32,28 @@ function populateTable(){
 		$('#userList table tbody').html(tableContent);
 	})
 }
+
+// show user information
+function showUserInfo(event){
+
+	// prevent link from firing
+	event.preventDefault();
+
+	//retreive username from link attr
+	var thisUserName = $(this).attr('rel');
+
+	//get index of object based on value
+
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+}
+
+
