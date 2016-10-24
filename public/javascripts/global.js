452e1279aacaf7a2938e95ddc481aa56546866c1
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-10-23 23:39:01 -0500

    JSON to HTML on page load in the table

---
+++ public/javascripts/global.js
@@ -0,0 +1,30 @@
+// userlist array to fill in the data
+var userListData = [];
+
+// DOM Ready?
+$(document).ready(function(){
+
+	// populate the user table on intial page load
+	populateTable();
+});
+
+function populationTable(){
+
+	var tableContent = ' ';
+
+	// jQuery AJAX call for JSON
+	$.getJson('/users/userlist', function(data){
+
+		// for each item in JSON, add a new row and cells to the table
+		$.each(data, function(){
+			tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+		});
+
+		//insert the whole string into the html table
+		$('#userList table tbody').html(tableContent);
+	})
+}
