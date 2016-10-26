a0d692e581beae57365fc80ea12d2edba2f44793
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-10-26 00:18:38 -0500

    adding POST method for new user

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,12 @@ router.get('/userlist', function(req,res){
 // NOTE: if you do an HTTP GET to /users/userlist, 
 //our server will return JSON that lists all of the users in the database. 
 
+router.post('/adduser', function(req, res){
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.insert(req.body, function(err, result){
+		res.send((err === null) ? {msg : ''} : {msg: err});
+	});
+});
+
 module.exports = router;
